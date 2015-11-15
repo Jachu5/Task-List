3fb355c9c641d1529d84067301bf09093caa6891
Jachu <acerezolun@gmail.com>
2015-11-16 00:55:20 +0100

    Command pattern implemented

---
+++ Wunderlist-demo/app/src/main/java/demo/wunderlist/alfredo_cerezo/wunderlist_demo/executor/Command.java
@@ -0,0 +1,9 @@
+package demo.wunderlist.alfredo_cerezo.wunderlist_demo.executor;
+
+/**
+ * Created by jachu on 16/11/15.
+ * <p>
+ * Interface to create executable interactors
+ */
+public interface Command extends Runnable {
+}
