29ea1cd7c41efc7e2e57f13cbe2247efddf4d22b
Jachu <acerezolun@gmail.com>
2015-11-22 20:37:29 +0100

    Added Custom image button, in a future it would be usefull

---
+++ Wunderlist-demo/app/src/main/java/demo/wunderlist/alfredo_cerezo/wunderlist_demo/android/animation/AnimationFactory.java
@@ -0,0 +1,24 @@
+package demo.wunderlist.alfredo_cerezo.wunderlist_demo.android.animation;
+
+import android.animation.ObjectAnimator;
+import android.widget.ImageButton;
+
+/**
+ * Created by jachu on 22/11/15.
+ */
+public class AnimationFactory {
+
+    public static ObjectAnimator getAddTaskCheckAnimationAppear(ImageButton checkButton) {
+        ObjectAnimator objectAnimatorButton = ObjectAnimator.ofFloat(checkButton, "translationX", 0f, 400f);
+        objectAnimatorButton.setDuration(1000);
+
+        return objectAnimatorButton;
+    }
+
+    public static ObjectAnimator getAddTaskCheckAnimationDisappear(ImageButton checkButton) {
+        ObjectAnimator objectAnimatorButton = ObjectAnimator.ofFloat(checkButton, "translationX", 0f, -400f);
+        objectAnimatorButton.setDuration(1000);
+
+        return objectAnimatorButton;
+    }
+}
